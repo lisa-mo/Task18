0eaba9fbc8dbe92be710c850bf3aceeb85968818
Lisa Mo <lisa.motorkina@gmail.com>
2020-10-02 23:20:10 +0300

    New method string in SecondClass

--- src/main/java/ForExampleNew/SecondClass.java
+++ src/main/java/ForExampleNew/SecondClass.java
@@ -1,7 +1,13 @@
 package ForExampleNew;
 
 public class SecondClass {
-    public static void main(int m) {
 
+    public static void main(String[] args) {
+        string("new");
+    }
+
+    private static String string(String s) {
+        s = null;
+        return s;
     }
 }
