b31df9a968bd3f0957d040001ca59f31605083cb
Lisa Mo <lisa.motorkina@gmail.com>
2020-10-03 02:02:11 +0300

    'No message' in First Class

--- src/main/java/FirstClass.java
+++ src/main/java/FirstClass.java
@@ -1,6 +1,6 @@
 public class FirstClass {
 //    Simple comment
     public static void main(String[] args) {
-        System.out.println("Changed");
+        System.out.println("No message");
     }
 }
