f45964b505f20e3db3a820028ae599987e2c0698
Lisa Mo <lisa.motorkina@gmail.com>
2020-10-03 04:02:15 +0300

    ForWork with new print and deleted comment

--- src/main/java/ForExampleNew/ForWork.java
+++ src/main/java/ForExampleNew/ForWork.java
@@ -1,10 +1,10 @@
 package ForExampleNew;
 // Union
 public class ForWork {
-    //No need for comment
+
     public static class FirstClass {
         public static void main(String[] args) {
-            System.out.println("First class");
+            System.out.println("ForWork");
         }
     }
 }
