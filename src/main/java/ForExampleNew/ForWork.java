0ad57229622095cdea6e1b2d09afe42802e64073
Lisa Mo <lisa.motorkina@gmail.com>
2020-10-03 04:03:16 +0300

    ForWork with new print and second  comment

--- src/main/java/ForExampleNew/ForWork.java
+++ src/main/java/ForExampleNew/ForWork.java
@@ -2,9 +2,10 @@ package ForExampleNew;
 // Union
 public class ForWork {
     //No need for comment
+//    Second comment
     public static class FirstClass {
         public static void main(String[] args) {
-            System.out.println("First class");
+            System.out.println("Change");
         }
     }
 }
