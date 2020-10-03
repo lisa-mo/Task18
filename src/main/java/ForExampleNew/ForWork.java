f60a73feb8cfa28df776202d776116618a7b7866
Lisa Mo <lisa.motorkina@gmail.com>
2020-10-03 04:16:33 +0300

    ForWork update

--- src/main/java/ForExampleNew/ForWork.java
+++ src/main/java/ForExampleNew/ForWork.java
@@ -1,11 +1,10 @@
 package ForExampleNew;
 // Union
 public class ForWork {
-    //No need for comment
-//    Second comment
+
     public static class FirstClass {
         public static void main(String[] args) {
-            System.out.println("Change");
+            System.out.println("ForWork");
         }
     }
 }
